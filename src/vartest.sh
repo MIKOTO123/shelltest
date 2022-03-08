762f5aa0a741c77c5384c7990b8d3079b6679729
cjlzzy <chenjialin@zzy.cn>
2022-03-08 17:11:35 +0800

    补充了变量部分的基础代码

--- src/vartest.sh
+++ src/vartest.sh
@@ -33,5 +33,58 @@ echo $greeting_2  $greeting_3
 
 
 
+string="abcd"
+echo ${#string}
+echo ${string:1:4}
 
 
+
+for skill in java php python
+do
+	echo $skill;
+done
+
+
+#查找子字符i或者o
+string="runoob is a great site"
+echo `expr index "$string" io`  # 输出 4
+
+
+
+
+array=(java php go python)
+
+echo ${array[0]}
+
+echo ${array[@]}
+
+
+echo ${array[*]}
+echo ${#array[*]}
+
+
+
+
+:<<EOF
+注释内容...
+注释内容...
+注释内容...
+EOF
+
+
+var="http://www.runoob.com/linux/linux-shell-variable.html"
+
+s1=${var%%t*}
+s2=${var%t*}
+s3=${var%%.*}
+s4=${var#*/}
+s5=${var##*/}
+:<<'
+关于字符串的截取%，#的使用方法
+原字符串为：http://www.runoob.com/linux/linux-shell-variable.html
+%%t*的效果：h
+%t*的效果：http://www.runoob.com/linux/linux-shell-variable.h
+%%.*的效果：http://www
+#*/的效果：/www.runoob.com/linux/linux-shell-variable.html
+##*/的效果：linux-shell-variable.html
+'
