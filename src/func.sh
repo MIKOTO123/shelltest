df91a63fed33ec99ff42ca1605994c16c46750bd
pikazo <pikazo512@gmail.com>
2022-03-14 16:51:44 +0800

    提交了有关函数部分的内容

---
+++ src/func.sh
@@ -0,0 +1,33 @@
+
+
+funWithReturn(){
+    echo "这个函数会对输入的两个数字进行相加运算..."
+    echo "输入第一个数字: "
+    read aNum
+    echo "输入第二个数字: "
+    read anotherNum
+    echo "两个数字分别为 $aNum 和 $anotherNum !"
+    return $(($aNum+$anotherNum))
+}
+funWithReturn
+echo "输入的两个数字之和为 $? !"  #注意这个$?,只有进紧邻的下一个语句才能获取,之后获取会失效,如果没有返回值的话,会返回0,shell里面0代表true
+
+
+
+
+funWithParam(){
+    echo "第一个参数为 $1 !"
+    echo "第二个参数为 $2 !"
+    echo "第十个参数为 $10 !"
+    echo "第十个参数为 ${10} !"
+    echo "第十一个参数为 ${11} !"
+    echo "参数总数有 $# 个!"
+    echo "作为一个字符串输出所有参数 $* !"
+}
+funWithParam 1 2 3 4 5 6 7 8 9 34 73
+
+
+
+
+
+
